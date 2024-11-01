cad1b618be67ffdc208e629a543607454f48a0aa
PedroThyrso <pedro.thyrso@atmodigital.com.br>
2024-11-01 10:19:22 -0300

    adicionando rota para logout

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\ChatController;
 
 Route::post('auth/login', [AuthController::class, 'login']);
 Route::post('auth/register', [AuthController::class, 'register']);
+Route::post('auth/logout', [AuthController::class, 'logout']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/user', function (Request $request) {
